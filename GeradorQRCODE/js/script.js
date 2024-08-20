955315bc4d0b6a6de635e1b8bc4a8e9ddd1fbc18
Julio Scanoni <juliosc4noni@gmail.com>
2024-08-20 18:45:15 -0300

    finalizado

--- GeradorQRCODE/js/script.js
+++ GeradorQRCODE/js/script.js
@@ -1,11 +1,43 @@
 const container = document.querySelector(".container");
 const qrCodeBtn = document.querySelector("#qr-form button");
 
+const qrCodeInput = document.querySelector("#qr-form input");
+
+const qrCodeImg = document.querySelector("#qr-code img")
+
+
 //Eventos
 function generateQrCode(){
-    console.log("teste");
+    const qrCodeInputValue = qrCodeInput.value;
+
+    if (!qrCodeInputValue) return;
+
+    qrCodeBtn.innerText = "Gerando código...";
+
+    qrCodeImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrCodeInputValue}`;
+
+    container.classList.add("active");
+
+    qrCodeImg.addEventListener("load", ()=>{
+        container.classList.add("active");
+        qrCodeBtn.innerText = "Código criado!";
+    });
+
 }
 
 qrCodeBtn.addEventListener("Click", () =>{
    generateQrCode(); 
 });
+
+qrCodeInput.addEventListener("Keydown", (e) =>{
+    if(e.code === "Enter")
+        generateQrCode();
+ });
+
+ // Limpar área dp QR Code
+ qrCodeInput.addEventListener("keyup", ()=>{
+    if (!qrCodeInput.value) {
+        container.classList.remove("active");
+        qrCodeBtn.innerText = "Gerar QR";
+    }
+ })
