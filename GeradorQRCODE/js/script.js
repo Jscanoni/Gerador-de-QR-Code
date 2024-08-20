9359a7f048d359edc416017a3b76e6ae66a1b326
Julio Scanoni <juliosc4noni@gmail.com>
2024-08-20 00:10:10 -0300

    alterações

---
+++ GeradorQRCODE/js/script.js
@@ -0,0 +1,11 @@
+const container = document.querySelector(".container");
+const qrCodeBtn = document.querySelector("#qr-form button");
+
+//Eventos
+function generateQrCode(){
+    console.log("teste");
+}
+
+qrCodeBtn.addEventListener("Click", () =>{
+   generateQrCode(); 
+});
--- GeradorQRCODE/js/scripts.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const scripts = () => {
-  return (
-    <div>scripts</div>
-  )
-}
-
-export default scripts
